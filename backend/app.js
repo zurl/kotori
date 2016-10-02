6508b914b36892813c2ca761df5de259c7b8971d
zurl <zurl@live.com>
2016-10-02 11:00:23 +0800

    initialize backend with socket.io

---
+++ backend/app.js
@@ -0,0 +1,7 @@
+/**
+ *  @file
+ *  @author Zhang Chengyi <zurl@live.com>
+ *  Created at 2016-10-02
+ */
+"use strict";
+
---
+++ backend/index.js
@@ -0,0 +1,5 @@
+// index.js 
+// 用于引入babel，并且启动app.js
+
+require("babel-core/register");
+require("./app.js");
